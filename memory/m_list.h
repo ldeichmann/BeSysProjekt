478f787b0f3290eabec421ba9a6ee76a553f02ca
vguentel <vguentel@googlemail.com>
2015-12-15 16:02:18 +0100

    added headers for mList and ProcessQueue

---
+++ memory/m_list.h
@@ -0,0 +1,53 @@
+/*
+Implementation von mList to manage Memory in SimOS
+*/
+
+#ifndef __M_LIST__
+#define __M_LIST__
+
+#include "bs_types.h"
+
+
+typedef enum{
+	free, used
+}mType;
+
+typedef struct{
+	mType type;
+	unsigned pid;
+	int start;
+	int length;
+	mListNode head;
+}mListNode;
+
+typedef struct{
+	mListNode head;
+	int length;
+}mList;
+
+//function Prototypes
+
+//makes new MListNode
+mListNode* makeMListNode();
+
+//makes new MList
+mList* makeMList();
+
+//adds Process to list returns false if not enough space; returns true if item was added
+Boolean addProcess(mList list, PCB_t process);
+
+//removes Process from List
+void removeProvess(mList list, PCB_t process);
+
+//compacts list
+void compact(mList list);
+
+//merges two free Nodes into one
+mListNode merge(mListNode a, mListNode b);
+
+//merges all adjactend free Nodes in mList
+void mergeAll(mList list);
+
+
+
+#endif /*__M_LIST__*/
---
+++ memory/process_queue.h
@@ -0,0 +1,41 @@
+/*
+PROCESS_QUEUE struct incl. Nodes for managing Process that dont fit into Memory into Queue
+*/
+
+#ifndef __PROCESS_QUEUE__
+#define __PROCESS_QUEUE__
+
+#include "bs_types.h"
+
+//QueueNode
+typedef struct{
+	PCB_t content;
+	processQueueNode next;
+}processQueueNode;
+
+//Process Queue
+typedef struct{
+	processQueueNode head;
+	processQueueNode tail;
+	int length;
+}processQueue;
+
+//Function Prototypes
+
+//creates new processQueueNode
+processQueueNode* makeProcessQueueNode();
+
+//Creates new ProcessQueue
+processQueue* makeProcessQueue();
+
+//Adds new Item to the Queue
+void enqueue(processQueue queue, PCB_t process);
+
+//removes and returns Item from Queue
+PCB_t dequeue(processQueue queue);
+
+//returns true if queue is Empty
+Boolean isEmpty(processQueue queue);
+
+
+#endif /*__PROCESS_QUEUE__*/
